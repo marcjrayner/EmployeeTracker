df1cf94e6daa1ab3a75d324729ada00a0b6b44d3
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-20 15:02:53 +0000

    lab finished

--- src/main/java/com/codeclan/example/EmployeeTracker/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/example/EmployeeTracker/controllers/EmployeeController.java
@@ -1,4 +1,35 @@
 package com.codeclan.example.EmployeeTracker.controllers;
 
+import com.codeclan.example.EmployeeTracker.models.Employee;
+import com.codeclan.example.EmployeeTracker.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value="/employees")
 public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+    
+    //index
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    //show
+    @GetMapping("{id}")
+    public Optional<Employee> getEmployee(@PathVariable long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteEmployee(@PathVariable long id) {
+        employeeRepository.deleteById(id);
+    }
+
 }
--- src/test/java/com/codeclan/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.codeclan.example.EmployeeTracker;
 
+import com.codeclan.example.EmployeeTracker.models.Employee;
+import com.codeclan.example.EmployeeTracker.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeTrackerApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void createEmployee() {
+		Employee employee = new Employee("John Smith", 18, 12, "john@smith.com");
+		Employee employee2 = new Employee("Jane Smith", 18, 13, "jane@smith.com");
+		employeeRepository.save(employee);
+		employeeRepository.save(employee2);
+	}
+
 
 }
