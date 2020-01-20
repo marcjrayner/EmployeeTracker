ba1cbade4922e03032e736625916777a669a67e5
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-20 14:14:50 +0000

    completes employee w/ annotations and repository to talk to db

---
+++ src/test/java/com/codeclan/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.EmployeeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
